eb1ab6aead34265cec0a029a0e8a764c1a01874c
Mohammed Kutubji <mohdmk@Mohammeds-MacBook-Pro.local>
2020-11-10 01:08:54 +0400

    updated the images

--- lessons.js
+++ lessons.js
@@ -3,7 +3,7 @@ let lessons = [
       "id": 1001,
       "title": "Mathematics",
       "price": 100.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://lh3.googleusercontent.com/proxy/e7J0h-6YeU0cchfXPWRPjzMx_-OaTxD-7bWMVX-l4_Z-1QipYxaKK9vVmJ2y5JTWYdm75cZiQeVUcsEp_xwMjSQFrYxfgwE",
       "location": "London",
       "spaces": 5,
       "rating": 2
@@ -12,7 +12,7 @@ let lessons = [
       "id": 1002,
       "title": "English",
       "price": 200.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://llo9inje9d30z3b8yleydwvf-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/English-for-Beginners-3-512x288-1.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
@@ -21,7 +21,7 @@ let lessons = [
       "id": 1003,
       "title": "Biology",
       "price": 250.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://cdn.medicalfuturist.com/wp-content/uploads/2019/10/DNA-MARYLAND-POLICE_V2-512x288.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
@@ -30,7 +30,7 @@ let lessons = [
       "id": 1004,
       "title": "Physics",
       "price": 400.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://tangiblejs.com/wp-content/uploads/2017/08/matterjs-512x288.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
@@ -39,16 +39,16 @@ let lessons = [
       "id": 1005,
       "title": "Chemistry",
       "price": 300.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://pnthn.perkinelmer.com/wp-content/uploads/2020/08/Demonstration-of-Signals-Screening-512x288-1.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
     },
     {
       "id": 1006,
-      "title": "Economics",
+      "title": "Finance",
       "price": 325.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://www.varchev.com/wp-content/uploads/2018/07/dollar-down-512x288.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
@@ -57,7 +57,7 @@ let lessons = [
       "id": 1007,
       "title": "History",
       "price": 450.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://www.open.edu/openlearn/ocw/pluginfile.php/1149651/mod_oucontent/oucontent/58983/c41bbddc/f30395a2/a344_2017j_vid057-512x288.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
@@ -66,7 +66,7 @@ let lessons = [
       "id": 1008,
       "title": "Geography",
       "price": 170.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://geography.fsu.edu/wp-content/uploads/2016/02/hurricane-floyd.jpg.adapt_.945.1-512x288.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
@@ -75,7 +75,7 @@ let lessons = [
       "id": 1009,
       "title": "Accounts",
       "price": 220.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://3b99b4b1b1eeb67b6cfed986-1j19geicz9gb29oiuecu.netdna-ssl.com/wp-content/uploads/Calculator-paperwork-accountant-512x288.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
@@ -84,7 +84,7 @@ let lessons = [
       "id": 1010,
       "title": "Business",
       "price": 180.00,
-      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "image": "https://media.uptopcorp.com/wp-content/uploads/2019/04/3-Examples-Business-Problems-Solved-UX-Design-512x288.jpg",
       "location": "London",
       "spaces": 5,
       "rating": 3
