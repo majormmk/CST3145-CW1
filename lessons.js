e7291cff2140ea8d3c433ff4ba6c586e9a238522
Mohammed Kutubji <mohdmk@Mohammeds-MacBook-Pro.local>
2020-11-09 18:31:55 +0400

    init

---
+++ lessons.js
@@ -0,0 +1,93 @@
+let lessons = [
+    {
+      "id": 1001,
+      "title": "Mathematics",
+      "price": 100.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 2
+    },
+    {
+      "id": 1002,
+      "title": "English",
+      "price": 200.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Biology",
+      "price": 250.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1004,
+      "title": "Physics",
+      "price": 400.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1005,
+      "title": "Chemistry",
+      "price": 300.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1006,
+      "title": "Economics",
+      "price": 325.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1007,
+      "title": "History",
+      "price": 450.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1008,
+      "title": "Geography",
+      "price": 170.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1009,
+      "title": "Accounts",
+      "price": 220.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    },
+    {
+      "id": 1010,
+      "title": "Business",
+      "price": 180.00,
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
+      "location": "London",
+      "spaces": 5,
+      "rating": 3
+    }
+]
+
