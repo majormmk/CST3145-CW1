e7224d16f081fb0b4c07692dcde46bbb6edd9734
Mohammed Kutubji <mohdmk@Mohammeds-MacBook-Pro.local>
2020-11-10 16:44:32 +0400

    adding sorting dropdowns

--- lessons.js
+++ lessons.js
@@ -3,9 +3,9 @@ let lessons = [
       "id": 1001,
       "title": "Mathematics",
       "price": 100.00,
-      "image": "https://lh3.googleusercontent.com/proxy/e7J0h-6YeU0cchfXPWRPjzMx_-OaTxD-7bWMVX-l4_Z-1QipYxaKK9vVmJ2y5JTWYdm75cZiQeVUcsEp_xwMjSQFrYxfgwE",
+      "image": "https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 6,
       "rating": 2
     },
     {
@@ -14,7 +14,7 @@ let lessons = [
       "price": 200.00,
       "image": "https://llo9inje9d30z3b8yleydwvf-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/English-for-Beginners-3-512x288-1.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 7,
       "rating": 3
     },
     {
@@ -23,7 +23,7 @@ let lessons = [
       "price": 250.00,
       "image": "https://cdn.medicalfuturist.com/wp-content/uploads/2019/10/DNA-MARYLAND-POLICE_V2-512x288.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 10,
       "rating": 3
     },
     {
@@ -32,7 +32,7 @@ let lessons = [
       "price": 400.00,
       "image": "https://tangiblejs.com/wp-content/uploads/2017/08/matterjs-512x288.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 8,
       "rating": 3
     },
     {
@@ -41,7 +41,7 @@ let lessons = [
       "price": 300.00,
       "image": "https://pnthn.perkinelmer.com/wp-content/uploads/2020/08/Demonstration-of-Signals-Screening-512x288-1.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 4,
       "rating": 3
     },
     {
@@ -50,7 +50,7 @@ let lessons = [
       "price": 325.00,
       "image": "https://www.varchev.com/wp-content/uploads/2018/07/dollar-down-512x288.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 2,
       "rating": 3
     },
     {
@@ -59,7 +59,7 @@ let lessons = [
       "price": 450.00,
       "image": "https://www.open.edu/openlearn/ocw/pluginfile.php/1149651/mod_oucontent/oucontent/58983/c41bbddc/f30395a2/a344_2017j_vid057-512x288.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 9,
       "rating": 3
     },
     {
@@ -77,7 +77,7 @@ let lessons = [
       "price": 220.00,
       "image": "https://3b99b4b1b1eeb67b6cfed986-1j19geicz9gb29oiuecu.netdna-ssl.com/wp-content/uploads/Calculator-paperwork-accountant-512x288.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 7,
       "rating": 3
     },
     {
@@ -86,7 +86,7 @@ let lessons = [
       "price": 180.00,
       "image": "https://media.uptopcorp.com/wp-content/uploads/2019/04/3-Examples-Business-Problems-Solved-UX-Design-512x288.jpg",
       "location": "London",
-      "spaces": 5,
+      "spaces": 7,
       "rating": 3
     }
 ]
